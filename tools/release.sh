d1b94acbd776aa3a92935a2533d641cf6686acc0
ZarTeK Creole <ZarTeK.Creole@gmail.com>
2022-07-24 09:40:04 +0000

    add changelog

--- tools/release.sh
+++ tools/release.sh
@@ -11,11 +11,20 @@ git pull
 git submodule sync --recursive
 git submodule update --init --recursive
 
-echo "<html><ul>" > change-notes.html
-git log `git describe --tags --abbrev=0`..HEAD --no-merges --oneline --pretty=format:"<li>%h %s (%an)</li>" >> change-notes.html
-echo "</ul></html>" >> change-notes.html
+echo "<html>
+  <table>
+    <tr>
+      <th>date</th>
+      <th>commit</th>
+      <th>message</th>
+      <th>auteur</th>
+    </tr>" > change-notes.html
+git log `git describe --tags --abbrev=0`..HEAD --no-merges --oneline --pretty=format:"<tr>%n      <td>%cs</td>%n      <td>%h</td>%n      <td><a href="https://github.com/ZarTek-Creole/TCL-ClaraServ/commit/%h">%s</a></td>%n      <td>%an</td>%n    </tr>" >> change-notes.html
+echo "  </table>
+</html>" >> change-notes.html
 git add .  
 git commit -am "$*"
 git push
 cd $DIR_CURRENT
+
 
